26753c175d0a0c78dc362a8c1d4a923f20d17d2a
lululufr <lucas.lm.miller@gmail.com>
2024-10-06 16:54:15 +0200

    Fix - keybind

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -32,7 +32,7 @@ return{
       -- Keymap
       vim.keymap.set('n', '<space>k', vim.lsp.buf.hover, {})
       vim.keymap.set('n', '<space>a', vim.lsp.buf.definition, {})
-      vim.keymap.set('n', 'gd', vim.lsp.buf.code_action, {})
+      vim.keymap.set('n', 'ca', vim.lsp.buf.code_action, {})
 
 
 
