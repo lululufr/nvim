fe0323ff5ebda2e2eb9162ff4c4396c259f1cfa4
lululufr <lucas.lm.miller@gmail.com>
2024-10-06 17:12:10 +0200

    file activation

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,5 +1,3 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
-
 -- Customize Mason plugins
 
 ---@type LazySpec
@@ -11,6 +9,10 @@ return {
     opts = {
       ensure_installed = {
         "lua_ls",
+        "pyright",
+        "clangd",
+        "ast_grep",
+
         -- add more arguments for adding more language servers
       },
     },
